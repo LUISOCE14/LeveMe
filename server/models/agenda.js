5a0867a9f21f379d6d36550f09e4adecd0c61efc
LUISOCE14 <oceguedalopezluis@gmail.com>
2024-08-09 15:39:49 -0600

    correcion

--- server/models/agenda.js
+++ server/models/agenda.js
@@ -1,1 +1,14 @@
 import mongoose from "mongoose";
+
+const agendaSchema = new mongoose.Schema({
+  actividades: [
+    {
+      actividad: String,
+      completada: Boolean,
+      required: true,
+    }
+  ]
+});
+
+export const AgendaModel = mongoose.model("Agenda", agendaSchema);
+
